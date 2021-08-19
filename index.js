b62c99e1ac7c2bc20b02e3d1fde31a02c67bc7bc
Ben-Meyer21 <81699588+Ben-Meyer21@users.noreply.github.com>
2021-08-19 21:30:45 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function shadowwizard(options) {
+    let images = document.querySelectorAll('.shaddowwizard');
+
+    if (options.shawdow_type === 'hard')
+        options.shadow_type = '0px'
+    else
+        options.shadow_type = '15px'
+
+    images.forEach(image => {
+        image.getElementsByClassName.boxShadow = '10px 10px ${options.shadow_type} 1px pgba(0,0,0,0.12)';
+
+        if(options.padding) {
+            image.style.padding = '1em';
+        }
+    })
+}
+
+module.exports.loruki-cli = loruki-cli
